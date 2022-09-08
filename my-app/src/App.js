909843da6c16236c1f17d4271521c5d0b1b25d44
M-Rahul1 <rahul2001ramesh@gmail.com>
2022-09-08 18:43:23 +0530

    v 13 done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,19 +2,32 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
+import Alert from './components/Alert';
 
  
 function App() {
   const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null); // Whether dark mode is enabled or not
 
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
   const toggleMode = ()=>{
     if(mode === 'light'){
       setMode('dark');
       document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode is enabled","success")
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("light mode is enabled","success")
     }
   }
   return (
@@ -22,7 +35,8 @@ function App() {
     {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
     {/* <Navbar/> */}
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-    <div className="container my-3">
+      <div className="container my-3">
+        <Alert alert={alert} />
     <TextForm heading="Enter the text to analyze below" mode={mode}/>
     </div>
     </> 
