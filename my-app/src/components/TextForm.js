2d66b6893bb91fba8da5bf5e16c091faef85062f
M-Rahul1 <rahul2001ramesh@gmail.com>
2022-09-10 19:51:06 +0530

    complete project

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -3,7 +3,6 @@ import React, {useState} from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = ()=>{
-        // console.log("Uppercase was clicked: " +  text);
         let newText = text.toUpperCase();
         setText(newText)
         props.showAlert("Converted to uppercase!", "success");
@@ -22,17 +21,12 @@ export default function TextForm(props) {
     }
 
     const handleOnChange = (event)=>{
-        // console.log("On change");
         setText(event.target.value) 
     }
 
     // Credits: A
     const handleCopy = () => {
-        console.log("I am copy");
-        var text = document.getElementById("myBox");
-        text.select();
-        text.setSelectionRange(0, 9999);
-        navigator.clipboard.writeText(text.value);
+        navigator.clipboard.writeText(text); 
         props.showAlert("Copied to Clipboard!", "success");
     }
 
@@ -49,7 +43,7 @@ export default function TextForm(props) {
     return (
         <>
         <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}> 
-            <h1>{props.heading}</h1>
+            <h1 className='mb-4'>{props.heading}</h1>
             <div className="mb-3"> 
             <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
             </div>
@@ -61,10 +55,10 @@ export default function TextForm(props) {
         </div>
         <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h2>Your text summary</h2>
-                {text.split(" ").filter((element) => { return element.length !== 0 }).length}
-                <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minute read </p>
-                <h2>Preview</h2>
-            <p>{text.length>0?text:"Nothing to preview"}</p>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+            <p>{0.008 *  text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text.length>0?text:"Nothing to preview!"}</p>
         </div>
         </>
     )
